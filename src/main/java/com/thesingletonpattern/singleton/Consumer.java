1a5174a00e15cf7bfd83f750d69be85584165ec1
Singleton06 <singletoncoding@gmail.com>
2020-03-15 22:27:22 -0500

    Fixing formatting to file on smaller lines

--- src/main/java/com/thesingletonpattern/singleton/Consumer.java
+++ src/main/java/com/thesingletonpattern/singleton/Consumer.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Consumer {
     public static void main(String[] args) { 
         List<Person> person = PersonService.INSTANCE.getPeopleWithName("Dustin");
-        person.stream().forEach(p -> System.out.println(p.getName() + " with id " + p.getId()));
+        person.stream().forEach(
+                p -> System.out.println(p.getName() + " with id " + p.getId()));
     }
 }
